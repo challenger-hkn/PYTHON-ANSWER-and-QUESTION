5651ba4f76da422a22911db8804c1ace26783d62
challenger <87441261+challenger-hkn@users.noreply.github.com>
2021-09-15 20:54:55 +0300

    Converter

---
+++ Measure Converter(Fahrenheit-celsius and miles-km).py
@@ -0,0 +1,17 @@
+# Task-1:
+# Write a short Python program that asks the user to enter Celsius temperature (it can be a decimal number),
+# converts the entered temperature into Fahrenheit degree and prints the result.
+#
+# Task-2:
+# Write a short Python program that asks the user to enter a distance (it can be a decimal number) in kilometers,
+# converts the entered distance into miles and prints the result.
+
+# Task-1:
+c = input("enter celcius temperature:")
+f = 33.8
+print("Fahrenheit degree :" , float(c) * f)
+
+# Task-2:
+k = input("enter kilometers:")
+m = 0.621371192
+print("miles :", float(k) * float(m))
